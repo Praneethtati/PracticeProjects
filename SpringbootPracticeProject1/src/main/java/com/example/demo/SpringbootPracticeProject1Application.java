7cb569d63642908f7a1e4281f15f8224ac5c89e0
Praneeth Reddy Tati <66408357+Praneethtati@users.noreply.github.com>
2023-07-15 12:10:06 +0530

    Add files via upload

---
+++ SpringbootPracticeProject1/src/main/java/com/example/demo/SpringbootPracticeProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPracticeProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPracticeProject1Application.class, args);
+	}
+
+}
